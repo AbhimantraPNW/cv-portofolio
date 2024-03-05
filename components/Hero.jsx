b8ed48f7b6a07d1aa734bb356371718832647865
AbhimantraPNW <abhimantrapnw04@gmail.com>
2024-03-05 19:52:29 +0700

    third commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -50,11 +50,6 @@ const Hero = () => {
                 </Button>
               </Link>
             </div>
-            {/* socials */}
-            <Socials
-              containerStyles="flex gap-x-6 mx-auto xl:mx-0"
-              iconStyles="text-foreground text-[22px] hover:text-primary transition-all"
-            />
           </div>
           {/* image */}
           <div className="hidden xl:flex relative">
