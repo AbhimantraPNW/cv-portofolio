c964403d6aef271139c57d9ddeae0cc7fa62807b
AbhimantraPNW <abhimantrapnw04@gmail.com>
2024-03-05 17:05:39 +0700

    first commit

--- components/ThemeToggler.jsx
+++ components/ThemeToggler.jsx
@@ -2,7 +2,7 @@
 
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
-import { Button } from "./ui/Button";
+import { Button } from "./ui/button";
 
 const ThemeToggler = () => {
   const { theme, setTheme } = useTheme();
